7cc36fbcaa51a48e9dc2651a8eff2f2373478dd7
Jose Sanchez <jpsantos.ggs@gmail.com>
2023-08-17 04:54:11 -0400

    some changes added to user saga and login saga to allow for successful logoff and login

--- src/redux/sagas/login.saga.js
+++ src/redux/sagas/login.saga.js
@@ -7,8 +7,6 @@ function* loginUser(action) {
     yield put({ type: 'CLEAR_LOGIN_ERROR' });
 
     // send the action.payload as the body
-    // the config includes credentials which
-    // allow the server session to recognize the user
     const response = yield fetch('/api/user/login', {
       method: "POST",
       headers: {
@@ -16,23 +14,28 @@ function* loginUser(action) {
       },
       body: JSON.stringify(action.payload),
     });
+
+    // Check if our network request was successful
     if (!response.ok) {
-      throw new Error("Network response was not OK");
+      const errorData = yield response.json();
+      if (response.status === 401) {
+        // If status code is 401, handle as unauthorized
+        throw new Error(errorData.message || "Unauthorized");
+      } else {
+        throw new Error("Network response was not OK");
+      }
     }
 
-    // after the user has logged in
-    // get the user information from the server
+    // after the user has logged in, get the user information from the server
     yield put({ type: 'FETCH_USER' });
   } catch (error) {
-    console.log('Error with user login:', error);
-    if (error.response.status === 401) {
-      // The 401 is the error status sent from passport
-      // if user isn't in the database or
-      // if the username and password don't match in the database
+    console.log('Error with user login:', error.message);
+
+    if (error.message === "Unauthorized") {
+      // If the error message is "Unauthorized", dispatch LOGIN_FAILED
       yield put({ type: 'LOGIN_FAILED' });
     } else {
-      // Got an error that wasn't a 401
-      // Could be anything, but most common cause is the server is not started
+      // Handle any other errors
       yield put({ type: 'LOGIN_FAILED_NO_CODE' });
     }
   }
@@ -48,9 +51,7 @@ function* logoutUser() {
       throw new Error("Network response was not OK");
     }
 
-    // now that the session has ended on the server
-    // remove the client-side user object to let
-    // the client-side code know the user is logged out
+    // now that the session has ended on the server, remove the client-side user object
     yield put({ type: 'UNSET_USER' });
   } catch (error) {
     console.log('Error with user logout:', error);
--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -10,7 +10,7 @@ function* fetchUser() {
     const user = yield response.json();
 
     // now that the session has given us a user object
-    // with an id and username set the client-side user object to let
+    // with an id and username, set the client-side user object to let
     // the client-side code know the user is logged in
     yield put({ type: 'SET_USER', payload: user });
   } catch (error) {
@@ -18,8 +18,24 @@ function* fetchUser() {
   }
 }
 
+// Worker saga for logout
+function* logoutUser() {
+  try {
+    // Assuming you have an API endpoint to log the user out.
+    const response = yield fetch('/api/logout', { method: 'POST' });
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+    // Dispatch an action to clear the user from the Redux store.
+    yield put({ type: 'UNSET_USER' });
+  } catch (error) {
+    console.log('User logout request failed', error);
+  }
+}
+
 function* userSaga() {
   yield takeLatest('FETCH_USER', fetchUser);
+  yield takeLatest('LOGOUT_REQUEST', logoutUser);
 }
 
 export default userSaga;
