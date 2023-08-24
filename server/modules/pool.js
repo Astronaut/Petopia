125e1ebc8ba1a43a4434912f63a06d1e886ba9cb
Jose Sanchez <jpsantos.ggs@gmail.com>
2023-08-23 21:49:20 -0400

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,10 +1,3 @@
-/* the only line you likely need to change is
-
- database: 'prime_app',
-
- change `prime_app` to the name of your database, and you should be all set!
-*/
-
 const pg = require('pg');
 const { v2: cloudinary } = require('cloudinary');
 
@@ -31,8 +24,8 @@ else {
         host: 'localhost',
         port: 5432,
         database: 'petopia',
-        user: "postgres",
-        password: "postgres"
+        // user: "postgres",
+        // password: "postgres",
     });
 }
 
